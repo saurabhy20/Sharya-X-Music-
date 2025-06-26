0da83a012e450bec29b48e48430a8efee881e453
saurabhy20 <82542283+saurabhy20@users.noreply.github.com>
2025-06-26 23:01:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+from dotenv import load_dotenv
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+load_dotenv()
+
+class Config:
+    API_ID = int(os.getenv("API_ID", 12345))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    OWNER_ID = int(os.getenv("OWNER_ID", 0))
+    SESSION = os.getenv("SESSION_STRING", "")
+    CHAT_ID = int(os.getenv("MUSIC_CHAT_ID", -10012345))
